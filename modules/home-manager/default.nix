9c411c535b3f67a7b327cba7baa67cefcea2f346
eswanson224 <eswanson224@proton.me>
2025-08-07 01:55:08 -0600

    more cleanup

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -3,13 +3,11 @@
 {
   imports =
     [
-      # ./alacritty.nix
       ./brave.nix
       ./easyeffects.nix
       ./emacs.nix
       ./firefox
       ./hyprland
-      # ./i3
       ./lutris.nix
       ./mpv
       ./thunderbird.nix
--- modules/nixos/mounts.nix
+++ modules/nixos/mounts.nix
@@ -4,6 +4,7 @@
   users.users.erik.packages = with pkgs; [
     cifs-utils
   ];
+
   fileSystems."/mnt/media" = {
     device = "//192.168.4.23/media";
     fsType = "cifs";
@@ -11,6 +12,7 @@
       automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
     in ["${automount_opts},credentials=/etc/nixos/smb-secrets"];
   };
+
   fileSystems."/mnt/erik" = {
     device = "//192.168.4.23/erik";
     fsType = "cifs";
@@ -18,6 +20,7 @@
       automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
     in ["${automount_opts},credentials=/etc/nixos/smb-secrets"];
   };
+
   fileSystems."/mnt/windows" = {
     device = "/dev/disk/by-uuid/F4523CC1523C89FE";
     fsType = "ntfs";
