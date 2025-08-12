1bfb62c3ea3444eab663b91a7834de66a898a6e2
eswanson224 <eswanson224@proton.me>
2025-08-12 15:54:21 -0600

    hyprlock

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -19,7 +19,7 @@ in
     enable = true;
   };
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
-  services.displayManager.ly.enable = true;
+  # services.displayManager.ly.enable = true;
 
   users.users.erik.packages = with pkgs; [
     cider
