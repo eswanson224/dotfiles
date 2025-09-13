c48cd720892e6d6f82395d68852be32a7778a7a6
eswanson224 <eswanson224@proton.me>
2025-09-13 12:57:30 -0600

    actually added obs

--- modules/home-manager/obs.nix
+++ modules/home-manager/obs.nix
@@ -1,5 +1,13 @@
 { config, lib, pkgs, ... }:
 
 {
+  programs.obs-studio = {
+    enable = true;
 
+    package = (
+      pkgs.obs-studio.override {
+        cudaSupport = true;
+      }
+    );
+  };
 }
