59b7085894e2dc4c27e20ebd51bb2d8403dd054e
eswanson224 <eswanson224@proton.me>
2025-10-04 19:49:19 -0600

    changed to master

--- modules/home-manager/terminal/git.nix
+++ modules/home-manager/terminal/git.nix
@@ -6,7 +6,7 @@
     userName = "eswanson224";
     userEmail = "eswanson224@proton.me";
     extraConfig = {
-      init = { defaultBranch = "main"; };
+      init = { defaultBranch = "master"; };
       push = { autoSetupRemote = true; };
     };
   };
