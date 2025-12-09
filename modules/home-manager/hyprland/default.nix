84e8cf795da69f00da809586786b39e68c89fbb6
eswanson224 <eswanson224@proton.me>
2025-12-08 17:11:05 -0700

    update

--- modules/home-manager/hyprland/default.nix
+++ modules/home-manager/hyprland/default.nix
@@ -93,7 +93,7 @@ in
         "AQ_DRM_DEVICES,/dev/dri/card1:/dev/dri/card0"
       ];
       monitor = [
-        "DP-2, 2560x1440@240, 0x0, 1"
+        "DP-2, 2560x1440@240, 0x0, 1.25"
         "eDP-1, 2560x1600@60, auto, 1.6"
         ", preferred, auto, 1"
       ];
