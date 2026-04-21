df9f840339a5e314ba0ccd8814f958a5284fe045
eswanson224 <erik@swanso.nz>
2026-04-20 20:17:32 -0600

    deadbeef plugins

--- modules/home-manager/desktop/default.nix
+++ modules/home-manager/desktop/default.nix
@@ -1,18 +1,12 @@
 { pkgs, ... }:
 
+let
+  ddbDiscordPlugin = pkgs.fetchzip {
+    url = "https://github.com/kuba160/ddb_discord_presence/releases/download/v1.7/linux-7550631.zip";
+    sha256 = "sha256-X+G5DpI3RUg8Dn0gwXqhI4pVfx7seBf06iptU4TG6HA="; 
+  };
+in
 {
-  home.packages = with pkgs; [
-    libreoffice
-    libimobiledevice
-    kdePackages.dolphin
-    kdePackages.kio-extras
-    (deadbeef-with-plugins.override {
-      plugins = with deadbeefPlugins; [
-        musical-spectrum
-      ];
-    })
-  ];
-
   imports = [
     # ./anki.nix
     # ./brave.nix
@@ -27,4 +21,21 @@
     ./vscodium.nix
     ./zathura.nix
   ];
+
+  home.packages = with pkgs; [
+    libreoffice
+    libimobiledevice
+    kdePackages.dolphin
+    kdePackages.kio-extras
+    (deadbeef-with-plugins.override {
+      plugins = with deadbeefPlugins; [
+        musical-spectrum
+      ];
+    })
+  ];
+
+  home.file.".local/lib/deadbeef" = {
+    source = ddbDiscordPlugin;
+    recursive = true;
+  };
 }
