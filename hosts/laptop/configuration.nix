5b603b3203d797fbb36d84c9f6c49267e9b996cf
eswanson224 <erik@swanso.nz>
2026-06-01 21:57:51 -0600

    update

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -14,15 +14,9 @@
       ];
       substituters = [
         "https://nix-community.cachix.org"
-        "https://hyprland.cachix.org"
-        "https://cache.nixos-cuda.org"
-        "https://nix-gaming.cachix.org"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-        "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
-        "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
       ];
       trusted-users = [
         "root"
