9850e2ad5a5b9a2dfcedddd281fb391bb703a960
eswanson224 <erik@swanso.nz>
2026-07-01 00:07:07 -0600

    emacs: removed unneded packages

--- modules/home-manager/desktop/emacs.nix
+++ modules/home-manager/desktop/emacs.nix
@@ -1,34 +1,17 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs; [
-    ispell
-    multimarkdown
-    nixfmt-classic
-    nodejs_24
-    shellcheck
-    tectonic
-  ];
-
   programs = {
     fzf.enable = true;
     ripgrep.enable = true;
     fd.enable = true;
   };
 
-  # services.emacs = {
-  #   enable = true;
-  # };
-
   programs.emacs = {
     enable = true;
     package = pkgs.emacs;
-    extraPackages = epkgs: [
-      epkgs.vterm
-      epkgs.pdf-tools
-      epkgs.tree-sitter-langs
-      epkgs.treesit-grammars.with-all-grammars
-      # (epkgs.treesit-grammars.with-grammars (grammars: [ grammars.tree-sitter-typst ]))
-    ];
+    # extraPackages = epkgs: [
+    #   epkgs.vterm
+    # ];
   };
 }
