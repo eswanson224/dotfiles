82e737163bfeeec967cb512141ca8943884067d6
eswanson224 <erik@swanso.nz>
2026-06-30 23:57:16 -0600

    helium: update

--- modules/nixos/packages/helium-browser.nix
+++ modules/nixos/packages/helium-browser.nix
@@ -2,7 +2,7 @@
 
 pkgs.appimageTools.wrapType2 rec {
   pname = "helium";
-  version = "0.13.5.1";
+  version = "0.13.6.1";
   src = pkgs.fetchurl {
     url = "https://github.com/imputnet/helium-linux/releases/download/${version}/helium-${version}-x86_64.AppImage";
     sha256 = "sha256-VAncL78nyXRRNUPZ2c0QudxFrfxy7tXE2NFN4teeezk=";
