a706581f6d3a418287c9e6b614ac047cfec11e29
eswanson224 <erik@swanso.nz>
2026-07-01 00:11:42 -0600

    lutris: removed unneeded hack

--- modules/home-manager/desktop/lutris.nix
+++ modules/home-manager/desktop/lutris.nix
@@ -1,22 +1,5 @@
 { pkgs, ... }:
 
-let
-  lutris-ldap-fix = (pkgs.lutris.override {
-    # Intercept buildFHSEnv to modify target packages
-    buildFHSEnv = args: pkgs.buildFHSEnv (args // {
-      multiPkgs = envPkgs:
-        let
-          # Fetch original package list
-          originalPkgs = args.multiPkgs envPkgs;
-
-          # Disable tests for openldap
-          customLdap = envPkgs.openldap.overrideAttrs (_: { doCheck = false; });
-        in
-        # Replace broken openldap with the custom one
-        builtins.filter (p: (p.pname or "") != "openldap") originalPkgs ++ [ customLdap ];
-    });
-  });
-in
 {
   home.packages = with pkgs; [
     adwaita-icon-theme
@@ -30,7 +13,6 @@ in
 
   programs.lutris = {
     enable = true;
-    package = lutris-ldap-fix;
     protonPackages = [ pkgs.proton-ge-bin ];
     winePackages = [ pkgs.wineWow64Packages.stable ];
   };
