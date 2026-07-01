3ec4cea9607e1d8625d6246c98fe94d6817912e9
eswanson224 <erik@swanso.nz>
2026-07-01 00:13:01 -0600

    removed dead service

--- modules/home-manager/services/default.nix
+++ modules/home-manager/services/default.nix
@@ -3,8 +3,5 @@
 {
   imports = [
     ./syncthing.nix
-    # HACK: kanshi disabled, broken with hyprland wlr-output-management events.
-    # Replaced by socket2 listener in hyprland/default.nix.
-    # ./kanshi.nix
   ];
 }
