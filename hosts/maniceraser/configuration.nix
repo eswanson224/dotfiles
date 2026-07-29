c54f2e7136f01e4479fd139f8a2bec198bd6da73
eswanson224 <erik@swanso.nz>
2026-07-29 22:25:54 +0000

    added maniceraser hardware

--- hosts/maniceraser/configuration.nix
+++ hosts/maniceraser/configuration.nix
@@ -4,8 +4,9 @@
   imports = [
     ../../modules/nixos/base
     ../../modules/nixos/profiles/desktop
-  ]
-  ++ lib.optional (builtins.pathExists ./hardware-configuration.nix) ./hardware-configuration.nix;
+    ./hardware-configuration.nix
+  ];
+  # ++ lib.optional (builtins.pathExists ./hardware-configuration.nix) ./hardware-configuration.nix;
 
   users.users.erik = {
     isNormalUser = true;
@@ -16,7 +17,21 @@
     ];
   };
 
+  services.hardware.openrgb = {
+    enable = true;
+    motherboard = "amd";
+  };
+
+  hardware.i2c.enable = true;
+
   networking.hostName = "maniceraser";
+
+  boot.kernelParams = [ "acpi_enforce_resources=lax" ];
+
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
 
   boot.loader = {
     systemd-boot.enable = true;
