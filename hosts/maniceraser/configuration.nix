689287124784fcdb7c560618c86b61f6797f1dac
eswanson224 <erik@swanso.nz>
2026-08-19 13:46:38 -0600

    added usb key for luks

--- hosts/maniceraser/configuration.nix
+++ hosts/maniceraser/configuration.nix
@@ -37,5 +37,16 @@ in
     kernelPackages = pkgs.linuxPackages_xanmod_stable;
   };
 
+  boot.initrd.availableKernelModules = [
+    "usb_storage"
+    "uas"
+  ];
+
+  boot.initrd.luks.devices."cryptroot" = {
+    keyFile = "/dev/disk/by-partuuid/951ad3ca-5d76-47a2-92a0-d10d057b9bce";
+    keyFileSize = 4096;
+    keyFileTimeout = 30;
+  };
+
   system.stateVersion = "26.05";
 }
