1338190802fa4c1b5c186aaf24deb6395f68a5ef
eswanson224 <erik@swanso.nz>
2026-08-20 19:40:54 -0600

    obsidian: remove unneeded hack

--- modules/home-manager/desktop/obsidian.nix
+++ modules/home-manager/desktop/obsidian.nix
@@ -2,9 +2,7 @@
 
 {
   home.packages = with pkgs; [
-    # electron 41.9.1 wasm regression freezes obsidian on launch;
-    # drop the override once nixpkgs electron is >= 41.10.2
-    (obsidian.override { electron = electron_42; })
+    obsidian
   ];
   # programs.obsidian = {
   #   enable = true;
