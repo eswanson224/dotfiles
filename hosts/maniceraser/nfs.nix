77c0933ba309d92a9e477da9342381781b62a542
eswanson224 <erik@swanso.nz>
2026-08-28 21:36:21 -0600

    nfs: added tailscale ip

--- hosts/maniceraser/nfs.nix
+++ hosts/maniceraser/nfs.nix
@@ -12,6 +12,14 @@
         "no_subtree_check"
         "fsid=0"
       ];
+
+      "100.91.120.77" = [
+        "ro"
+        "sync"
+        "root_squash"
+        "no_subtree_check"
+        "fsid=0"
+      ];
     };
 
     "/export/srv" = {
@@ -22,10 +30,8 @@
         "no_subtree_check"
         "mountpoint"
       ];
-    };
 
-    "/export/media" = {
-      "192.168.0.82" = [
+      "100.91.120.77" = [
         "ro"
         "sync"
         "root_squash"
